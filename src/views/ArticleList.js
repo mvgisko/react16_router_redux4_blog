000fbd4f4d28837f6107ae1bc1d900b09231fa17
Gisko Maksim <mvgisko@gmail.com>
2018-05-04 12:00:54 +0300

    added sort by title, author

--- src/views/ArticleList.js
+++ src/views/ArticleList.js
@@ -2,7 +2,7 @@
 * @Author: Gisko Maksim
 * @Date:   2018-04-20 17:56:47
 * @Last Modified by:   Gisko Maksim
-* @Last Modified time: 2018-05-03 20:19:25
+* @Last Modified time: 2018-05-04 12:00:23
 */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
@@ -14,8 +14,18 @@ import './articleList.css';
 class ArticleList extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { search: '' };
+		this.state = {
+			search: '',
+			sort: 'title_desc',
+		};
+		this.sort = [
+			{ value: 'title_desc', name: 'Title (desc)' },
+			{ value: 'title_asc', name: 'Title (asc)' },
+			{ value: 'userName_desc', name: 'Author (desc)' },
+			{ value: 'userName_asc', name: 'Author (asc)' },
+		];
 		this.handleSearch = this.handleSearch.bind(this);
+		this.handleSort = this.handleSort.bind(this);
 	}
 
 	componentDidMount() {
@@ -26,14 +36,31 @@ class ArticleList extends Component {
 		this.setState({ search: event.target.value.toLowerCase() });
 	}
 
+	handleSort(event) {
+		this.setState({ sort: event.target.value });
+	}
+
 	render() {
 		const { articles } = this.props;
+		const { search, sort } = this.state;
 
 		if (articles.loading) return <Spinner />;
 
 		const displayedArticles = articles.data.filter((el) => {
 			const searchValue = el.title.toLowerCase();
-			return searchValue.indexOf(this.state.search) !== -1;
+			return searchValue.indexOf(search) !== -1;
+		}).sort((prev, next) => {
+			const [field, sortDirec] = sort.split('_');
+
+			if (sortDirec === 'desc') {
+				if (prev[field] < next[field]) return 1;
+				if (prev[field] > next[field]) return -1;
+			} else if (sortDirec === 'asc') {
+				if (prev[field] > next[field]) return 1;
+				if (prev[field] < next[field]) return -1;
+			}
+
+			return 0;
 		});
 
 		return (
@@ -41,8 +68,18 @@ class ArticleList extends Component {
 				<div className="Items__nav">
 					<span>
 						<input type="search" placeholder="Search" onChange={this.handleSearch} />
+						Sort by:
+						<select name="sort" id="sortArticle" onChange={this.handleSort}>
+							{
+								this.sort.map((el) => {
+									const { value, name } = el;
+									return (
+										<option key={value} value={value}>{name}</option>
+									);
+								})
+							}
+						</select>
 					</span>
-					This is navigation for articles
 				</div>
 
 				<ul className="Items__list">
